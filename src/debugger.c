fe66e8f9e20c3d6b5d8b760b15fb4e7d01fa3e08
Ian-Marco-Moffett <spurious.vector@gmail.com>
2022-06-26 02:45:51 -0400

    refactor: cleaned up tabbing.

--- src/debugger.c
+++ src/debugger.c
@@ -8,22 +8,22 @@
 static int wait_status;
 
 static void exec_command(const char* command, pid_t child_pid) {
-	if (strncmp(command, "continue", 8) == 0) {
-		// Start execution of child process.
-		ptrace(PTRACE_CONT, child_pid, NULL, NULL);
-	}
+    if (strncmp(command, "continue", 8) == 0) {
+        // Start execution of child process.
+        ptrace(PTRACE_CONT, child_pid, NULL, NULL);
+    }
 }
 
 
 void debug_proc(pid_t pid) {
-	int options = 0;
-	waitpid(pid, &wait_status, options);
+    int options = 0;
+    waitpid(pid, &wait_status, options);
 
-	char buffer[50];
+    char buffer[50];
 
-	while (1) {
-		printf("nexdebug> ");
-		fgets(buffer, sizeof(buffer), stdin);
-		exec_command(buffer, pid);
-	}
+    while (1) {
+        printf("nexdebug> ");
+        fgets(buffer, sizeof(buffer), stdin);
+        exec_command(buffer, pid);
+    }
 }
